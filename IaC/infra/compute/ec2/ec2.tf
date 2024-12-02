5496f222fd53d0b4e3bd501127e05c6790d6414e
Sriman-COGNIWIDE <sriman.mayandi@cogniwide.com>
2024-12-02 18:16:35 +0530

    ec2-file changed

--- IaC/infra/compute/ec2/ec2.tf
+++ IaC/infra/compute/ec2/ec2.tf
@@ -40,17 +40,5 @@ module "vm_connect" {
   enable_app_vpc_peering_eg = true
   app_peer_cidr             = [data.terraform_remote_state.nets.outputs.app_subnet_cidr]
 
-  allow_rmt_via_bastion = true
-  bastion_ip            = module.vm_nginx.public_ip
-  bastion_private_key   = module.vm_nginx.tls_key
-  key                   = module.vm_connect.tls_key
-  bastion_username      = "ec2-user"
-  remote_exec_cmds = [
-    "curl -X POST http://${module.vm_app_1.private_ip}:5000/recipes -H 'Content-Type: application/json' -d '{\"name\": \"idly\", \"description\": \"a rice cake\"}'",
-    "curl -X POST http://${module.vm_app_2.private_ip}:5000/recipes -H 'Content-Type: application/json' -d '{\"name\": \"poori\", \"description\": \"a deep-fried  bread\"}'",
-    "curl http://${module.vm_app_2.private_ip}:5000/recipes"
-  ]
-
   deployment_mode = "terraform"
-  depends_on = [module.vm_bastion, module.vm_app_1, module.vm_app_2]
 }
